bd0f91f468854954745683ec528673c831afbc90
Rishabh Deo Pandey <70680319+rishabhdeo60@users.noreply.github.com>
2024-02-15 18:45:37 +0530

    Add files via upload
    Initial cocmmit

---
+++ src/main/java/com/example/MedecalStoreManagement/MedecalStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedecalStoreManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedecalStoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedecalStoreManagementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/service/MedicalService.java
@@ -0,0 +1,12 @@
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.repository.MedicalStoreRepository;
+
+@Service
+public class MedicalService {
+
+	private MedicalStoreRepository medicalStoreRepository;
+
+}
